f3365423451354b355706878a5e549ed35b85650
JJ <79458684+jeongdev@users.noreply.github.com>
2024-07-19 00:38:26 +0900

    feat: btns
    container 밖으로 뺌

--- src/pages/AvailablePage.jsx
+++ src/pages/AvailablePage.jsx
@@ -1,6 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Available from "../components/available/Available";
+import { DefaultWhiteBtn } from "../components/commons/buttons/Buttons";
 
 export default function AvailablePage() {
-  return <Available></Available>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Products</h2>
+
+        <div className="flex justify-end items-center mb-6">
+          <DefaultWhiteBtn type="button" onClick={() => navigate("/list/form")}>
+            등록
+          </DefaultWhiteBtn>
+        </div>
+
+        <article>
+          <Available />
+        </article>
+      </div>
+    </div>
+  );
 }
