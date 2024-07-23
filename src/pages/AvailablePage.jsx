3b127c94d3d1f0610e7be985720559ccc51fd1ac
JJ <79458684+jeongdev@users.noreply.github.com>
2024-07-24 01:03:53 +0900

    feat: available list GET

--- src/pages/AvailablePage.jsx
+++ src/pages/AvailablePage.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Available from "../components/available/Available";
 import { DefaultWhiteBtn } from "../components/commons/buttons/Buttons";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function AvailablePage() {
   const navigate = useNavigate();
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      try {
+        // get
+        const docRef = collection(db, "available");
+        const docSnap = await getDocs(docRef);
+
+        const newArr = [];
+        docSnap.forEach((e) => {
+          newArr.push({ ...e.data(), id: e.id });
+        });
+
+        setList(newArr);
+      } catch (error) {
+        console.error("Error getting document:", error);
+      }
+    })();
+  }, []);
 
   return (
     <div className="bg-white">
@@ -18,7 +40,7 @@ export default function AvailablePage() {
         </div>
 
         <article>
-          <Available />
+          <Available data={list} />
         </article>
       </div>
     </div>
