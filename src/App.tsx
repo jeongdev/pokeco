2d9ad4ec21487e0115171fd66c8bb5ff342bb5b7
jeongdev <jjeongvel@gmail.com>
2024-10-30 00:25:27 +0900

    build: typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/commons/header/Header";
+import Footer from "./components/commons/footer/Footer";
 import AvailablePage from "./pages/AvailablePage";
 import DetailAvailablePage from "./pages/DetailAvailablePage";
 import AvailableFormPage from "./pages/AvailableFormPage";
 import NotFound from "./components/commons/notFound/NotFound";
 
-function App() {
+const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/list" element={<AvailablePage />} />
         <Route path="/list/:id" element={<DetailAvailablePage />} />
         <Route path="/list/form" element={<AvailableFormPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
