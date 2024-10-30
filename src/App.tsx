0f4e5b0a8a92cae03a751da8718df7dd7d81a05e
jeongdev <jjeongvel@gmail.com>
2024-10-31 00:34:16 +0900

    feat: about

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/commons/header/Header";
+import Main from "./pages/Main";
 import Footer from "./components/commons/footer/Footer";
 import AvailablePage from "./pages/AvailablePage";
 import DetailAvailablePage from "./pages/DetailAvailablePage";
 import AvailableFormPage from "./pages/AvailableFormPage";
 import NotFound from "./components/commons/notFound/NotFound";
+import AboutPage from "./pages/AboutPage";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/list" element={<AvailablePage />} />
-        <Route path="/list/:id" element={<DetailAvailablePage />} />
-        <Route path="/list/form" element={<AvailableFormPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/list" element={<AvailablePage />} />
+          <Route path="/list/:id" element={<DetailAvailablePage />} />
+          <Route path="/list/form" element={<AvailableFormPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Main>
       <Footer />
     </BrowserRouter>
   );
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AboutPage() {
+  return <div>안녕하세요. Poke입니다</div>;
+}
