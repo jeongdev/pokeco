6ca321419000e5425dcea57ce2622f8268f00a40
jeongdev <jjeongvel@gmail.com>
2024-11-04 23:43:55 +0900

    style, design: footer
    tsx로 변경, 배경 추가

--- src/components/commons/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="fixed left-1/2 -translate-x-1/2 bottom-0 w-full py-4 max-w-screen-sm sm:shadow-xl">
-      <p className="text-center font-bold tracking-widest">P O K E C O</p>
-    </footer>
-  );
-}
